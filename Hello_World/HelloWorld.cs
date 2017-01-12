6e4ae92707d2ce99f8c89d96e233f4debf3f3eb2
codersyntax <contact@codersyntax.com>
2017-01-12 07:52:04 -0500

    First Commit

---
+++ Hello_World/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿/* Name: Hello_World
+ * Author: Derrick Minor
+ * Author Url: http://www.codersyntax.com
+ * Description: A 'Hello World' program in C#. This program displays the string 'Hello World' as well as an exit prompt.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
