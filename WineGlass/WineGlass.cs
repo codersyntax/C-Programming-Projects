59df37e7b3eaff4c4c7726f41bae3c1eb1dcebcf
codersyntax <contact@codersyntax.com>
2017-01-20 02:36:27 -0500

    Initial Commit

---
+++ WineGlass/WineGlass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XXXXXXXXXXXXXX");
+            Console.WriteLine(" X          X ");
+            Console.WriteLine("  X        X  ");
+            Console.WriteLine("   XXXXXXXX   ");
+            Console.WriteLine("      XX      ");
+            Console.WriteLine("      XX      ");
+            Console.WriteLine("    XXXXXX    \n");
+            Console.WriteLine("TADDDAAAAA WINEGLASS!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
